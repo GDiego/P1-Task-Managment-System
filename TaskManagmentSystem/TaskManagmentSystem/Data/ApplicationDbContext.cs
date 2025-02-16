50fc1e9a27f99d6b69f36d67ed83e36b6f494bb0
DESKTOP-NT8CJ3F\Gamer <myproject@gmail.com>
2025-02-16 16:42:08 -0300

    Agregar ApplicationDbContext y configuración de conexión a la base de datos

---
+++ TaskManagmentSystem/TaskManagmentSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagmentSystem.Data
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
--- TaskManagmentSystem/TaskManagmentSystem/Program.cs
+++ TaskManagmentSystem/TaskManagmentSystem/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TaskManagmentSystem.Data;
+
 namespace TaskManagmentSystem
 {
     public class Program
@@ -8,6 +12,9 @@ namespace TaskManagmentSystem
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddControllers();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
